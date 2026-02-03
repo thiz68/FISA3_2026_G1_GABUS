df3c14615217f8362b9729087d19495c5739be76
Thibaud GABUS <thibaud.gabus@viacesi.fr>
2026-02-03 15:18:36 +0100

    Implementing constructor

--- EasySave/EasySave.Core/Services/ConfigManager.cs
+++ EasySave/EasySave.Core/Services/ConfigManager.cs
@@ -4,7 +4,24 @@ using System.Text.Json;
 using EasySave.Core.Interfaces;
 using EasySave.Core.Models;
 
-public class ConfigManager
+//Saving and loading jobs configurations to a JSON file
+public class ConfigManager : IConfigManager
 {
+    //Path to configuration file
+    private readonly string _configFilePath;
     
+    //Constructor
+    public ConfigManager()
+    {
+        //Get AppData folder
+        var appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+
+        //Create app folder
+        var configDir = Path.Combine(appData, "EasySave");
+        
+        Directory.CreateDirectory(configDir);
+
+        //Set path to config file
+        _configFilePath = Path.Combine(configDir, "config.json");
+    }
 }

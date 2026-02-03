0100b3c09450d5d5a62693ce03ce96224b24ff7a
Clery <clery.gardes@gmail.com>
2026-02-03 11:42:49 +0100

    Add SaveType to Enums

---
+++ EasySave/EasySave.Core/Enums/SaveType.cs
@@ -0,0 +1,10 @@
+namespace EasySave.Core.Enums;
+
+// Type of backup
+public enum SaveType
+{
+    // Full backup (all files)
+    Full,
+    // Differential backup (edited/new files)
+    Differential,
+}
